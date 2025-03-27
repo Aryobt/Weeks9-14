da84231d42172a7825e5e49f7a4e3d54b24717da
Aryobarzan Taghavi <taghavia@shernet.sheridancollege.ca>
2025-03-27 11:26:33 -0400

    Coding Gym week 11

--- Assets/Scripts/Khight.cs
+++ Assets/Scripts/Khight.cs
@@ -6,6 +6,11 @@ public class Khight : MonoBehaviour
 {
     SpriteRenderer sr;
     Animator animator;
+    public AudioClip walkSound;
+    public AudioSource Soundmaker;
+    
+    public AudioSource AttacksoundM;
+    public AudioClip AtkSound;
     public float speed = 2;
     public bool canRun = true;
     // Start is called before the first frame update
@@ -13,6 +18,7 @@ public class Khight : MonoBehaviour
     {
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+      
     }
 
     // Update is called once per frame
@@ -22,22 +28,36 @@ public class Khight : MonoBehaviour
 
         sr.flipX = (direction < 0);
         animator.SetFloat("New Float", Mathf.Abs(direction));
+        
+        if(Input.GetKeyDown (0))
+        {
+
+        }
 
         if(Input.GetMouseButtonDown(0))
         {
             animator.SetTrigger("Attack");
             canRun = false;
+            AttacksoundM.clip = AtkSound;
+            AttacksoundM.Play();
         }
 
         if (canRun == true)
         {
             transform.position += transform.right * direction * speed * Time.deltaTime;
         }
+
     }
 
     public void AttackHasFinished()
     {
         Debug.Log("The attack animation just finished!");
         canRun = true;
+    }
+
+    public void PuttheFootDown()
+    {
+        Soundmaker.clip = walkSound;
+        Soundmaker.Play();
     }
 }
