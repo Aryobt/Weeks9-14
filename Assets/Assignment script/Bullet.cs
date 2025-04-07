177cad4ac1ac30b9421500615c64c0c336f0c5f4
Aryobt <aryobt2@gmail.com>
2025-04-07 12:23:55 -0400

    added bullet code

--- Assets/Assignment script/Bullet.cs
+++ Assets/Assignment script/Bullet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Bulletspawn : MonoBehaviour
 {
+    public Vector3 mousePos;
+     
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,12 @@ public class Bulletspawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            transform.position = mousePos;
+        }
     }
 }
