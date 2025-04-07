d4fbf684fe77d46230cf223d9dc3d5fddf11bd7d
Aryobt <aryobt2@gmail.com>
2025-04-07 17:40:52 -0400

    made bullets move

--- Assets/Assignment script/Bullet.cs
+++ Assets/Assignment script/Bullet.cs
@@ -1,23 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Bulletspawn : MonoBehaviour
 {
-  //  [Range(0f, 1f)]
-  //  public float t;
-   // public AnimationCurve bullet;
-
-   // public Vector2 start;
-   // public Vector2 end;
-
-    public Vector3 mousePos;
-
-   
-
   
-     
+   public float BulletSpeed;
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,16 +23,10 @@ public class Bulletspawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))// so if the mouse button is pressed change the position of the bullet to where the mouse is
-        {
-           mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-           mousePos.z = 0;
-           transform.position = mousePos;
-           
+        Vector3 pos = transform.position;
+        pos.x += BulletSpeed * Time.deltaTime;
 
-            //transform.position = Vector2.Lerp(start, end, bullet.Evaluate(t));
-        }
-
+        transform.position = pos;
        
     }
 }
