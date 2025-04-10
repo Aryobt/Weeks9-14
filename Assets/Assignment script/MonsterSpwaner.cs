d63b0eb9b6b0ca26a41f65d58d4b48d1d2834940
Aryobarzan Taghavi <taghavia@shernet.sheridancollege.ca>
2025-04-10 10:39:37 -0400

    Fixed the monster spawner and now it finally works

--- Assets/Assignment script/MonsterSpwaner.cs
+++ Assets/Assignment script/MonsterSpwaner.cs
@@ -7,39 +7,37 @@ public class MonsterSpwaner : MonoBehaviour
     //veriables to refrence the monster spawner game object
     public GameObject MonsterSpawner;
     public GameObject Monster;//the object that is being spawned by the game object
-    public GameObject newMonster;
+    //public GameObject newMonster;
 
-    public float spawnTime = 3f;//The veriable that handels the spawn time of each monster
+    public float spawnTime = 2f;//The veriable that handels the spawn time of each monster
 
     Coroutine SpawnerCoroutine;// setting a coroutine to spawn monsters
-    IEnumerator Spawn;
+    //IEnumerator Spawn;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnerCoroutine = StartCoroutine(MonsterRoutine());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+       SpawnerCoroutine = StartCoroutine(MonsterRoutine());
     }
 
     IEnumerator MonsterRoutine()
     {
         while (true)
         {
-            SpawnMonstersInfront();
+            SpawnMonsters();
             yield return new WaitForSeconds(spawnTime);
+            Debug.Log("Spawnmonsters");
         }
     }
 
-    public void SpawnMonstersInfront()
+    public void SpawnMonsters()
 
     {
-        Vector2 posToSpawn = new Vector3(Random.Range(-9.5f, 9.5f), 6.2f);
-        newMonster.transform.parent = MonsterSpawner.transform;
+        Vector2 posToSpawn = transform.position;
+        Vector3 rotationToSpawn = new Vector3(0, 180, 0);
+        GameObject newEnemy = Instantiate(Monster, posToSpawn, Quaternion.Euler(rotationToSpawn));
+        newEnemy.transform.parent = MonsterSpawner.transform;
+        Debug.Log("Spawnenemy");
     }
 }
